ed8a230c8f42817f30963a5003b45340b85f19fd
Papika0 <d_papismedovi@cu.edu.ge>
2022-08-29 14:29:39 +0400

    employee page html css almost done

--- App/script/getData.js
+++ App/script/getData.js
@@ -3,10 +3,10 @@ function getData(api, selectId) {
     const response = await fetch(api);
     const base = await response.json();
 
-    [base][0].data.map((val) => {
+    [base][0].data.map((char) => {
       var optionCreation = document.createElement("option");
-      optionCreation.text = val.name;
-      optionCreation.id = val.id;
+      optionCreation.text = char.name;
+      optionCreation.id = char.id;
       document.getElementById(selectId).appendChild(optionCreation);
     });
   }
@@ -16,25 +16,24 @@ function getData(api, selectId) {
 
 getData("https://pcfy.redberryinternship.ge/api/teams", "teams");
 
-async function getBase() {
+async function getPositions() {
   var getSelect = document.querySelectorAll(".teams");
   getSelect.forEach(function (option) {
-    option.addEventListener("change", async function countId(e) {
+    option.addEventListener("change", async function (e) {
       var target = e.target.id;
       selected = e.target.options[e.target.selectedIndex].id;
-      console.log(selected);
       const response = await fetch(
         "https://pcfy.redberryinternship.ge/api/positions"
       );
       const base = await response.json();
-      [base][0].data.map((val) => {
-        if (selected == val.team_id) {
+      [base][0].data.map((char) => {
+        if (selected == char.team_id) {
           var optionCreation = document.createElement("option");
-          optionCreation.text = val.name;
-          optionCreation.id = "option" + val.team_id;
+          optionCreation.text = char.name;
+          optionCreation.id = "option" + char.team_id;
           document.getElementById("positions").appendChild(optionCreation);
         } else {
-          element = document.getElementById("option" + val.team_id);
+          element = document.getElementById("option" + char.team_id);
           if (element !== null) {
             element.remove();
           }
@@ -44,4 +43,4 @@ async function getBase() {
   });
 }
 
-getBase();
+getPositions();
---
+++ App/script/validations.js
@@ -0,0 +1,6 @@
+let firstName = document.getElementById("Name");
+let lastName = document.getElementById("Lastname");
+let teams = document.getElementById("teams");
+let positions = document.getElementById("positions");
+let email = document.getElementById("Email");
+let phone = document.getElementById("Phone");
